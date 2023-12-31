f93ca69dd519004256c73fe9cef63acda9403eb9
JIM440 <118163334+JIM440@users.noreply.github.com>
2023-12-31 18:05:05 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
-const menu = document.querySelector('nav .bi-list')
-const close = document.querySelector('nav .bi-x')
+const menu = document.querySelector('.menu-button')
 const mobileNav = document.querySelector('.mobile-nav')
 
 menu.addEventListener('click', ()=>{
-    mobileNav.classList.add('active')
+    mobileNav.classList.toggle('active')
+    menu.classList.toggle('clicked')
 })
-close.addEventListener('click', ()=>{
-    mobileNav.classList.remove('active')
+
+const navDropDowns = document.querySelectorAll('.mobile-nav li > div.drop-down')
+const mobileNavLinks = document.querySelectorAll('.mobile-nav ul li > a.down')
+mobileNavLinks.forEach((link, index) => {
+    link.addEventListener('click', (event)=>{
+                event.preventDefault()
+console.log(index)
+        link.classList.toggle('active')
+        navDropDowns[index].classList.toggle('active')
+        })
+})
+
+// faqs section
+
+const faqs = document.querySelectorAll('.faqs')
+
+faqs.forEach(faq => {
+
+    faq.addEventListener('click', () => { 
+
+        if(faq.classList.contains("active")){
+            faq.classList.remove('active')   
+        }
+
+        else{
+                  // remove all active classes
+    faqs.forEach(faq1 => {
+        faq1.classList.remove('active')
+})
+// toogle active
+        faq.classList.add('active')  
+        }
+    })
 })
