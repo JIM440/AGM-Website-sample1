f56f537bb87f83c565859841df23d068ed11d567
JIM440 <118163334+JIM440@users.noreply.github.com>
2024-01-03 15:53:36 +0100

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,6 @@ menu.addEventListener('click', ()=>{
 
 const navDropDowns = document.querySelectorAll('.mobile-nav li > div.drop-down')
 const mobileNavLinks = document.querySelectorAll('.mobile-nav ul li > a.down')
-console.log(mobileNavLinks)
 mobileNavLinks.forEach((link, index) => {
     link.addEventListener('click', (event)=>{
                 event.preventDefault()
@@ -16,4 +15,27 @@ console.log(index)
         link.classList.toggle('active')
         navDropDowns[index].classList.toggle('active')
         })
+})
+
+// faqs section
+
+const faqs = document.querySelectorAll('.faqs')
+
+faqs.forEach(faq => {
+
+    faq.addEventListener('click', () => { 
+
+        if(faq.classList.contains("active")){
+            faq.classList.remove('active')   
+        }
+
+        else{
+                  // remove all active classes
+    faqs.forEach(faq1 => {
+        faq1.classList.remove('active')
+})
+// toogle active
+        faq.classList.add('active')  
+        }
+    })
 })
